4a10b32ae5f0752be6520acfac53bbf706c780d9
leokao960811 <117443600+leokao960811@users.noreply.github.com>
2026-03-14 12:23:11 +0800

    First Homework

---
+++ HW1.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Shape
+{
+    protected:
+        int x,y;
+
+    public:
+        Shape(int x, int y)
+        {
+            this->x = x;
+            this->y = y;
+        }
+
+        virtual void display() = 0;
+        virtual ~Shape(){}
+};
+
+class Circle: public Shape
+{
+    private:
+        int radius; //hidden from others
+
+    public:
+        Circle(int x, int y, int r): Shape(x,y), radius(r){}
+        void display() override
+        {
+            cout << "Circle at ("<< x << ","<< y <<")\n Radius: " << radius << endl;
+        }
+};
+
+class Triangle: public Shape
+{
+    private:
+        int angle; //hidden from others
+
+    public:
+        Triangle(int x, int y, int r): Shape(x,y), angle(r){}
+        void display() override
+        {
+            cout << "Triangle at ("<< x << ","<< y <<")\n Angle: " << angle << endl;
+        }
+};
+
+class Square: public Shape
+{
+    private:
+        int area; //hidden from others
+
+    public:
+        Square(int x, int y, int r): Shape(x,y), area(r){}
+        void display() override
+        {
+            cout << "Square at ("<< x << ","<< y <<")\n Area: " << area << endl;
+        }
+};
+
+int main()
+{
+
+    vector <Shape*> shapes;
+
+    
+    shapes.push_back(new Square(5, 5, 25));
+    shapes.push_back(new Triangle(2, 3, 60));
+    shapes.push_back(new Circle(9, 8, 10));
+    shapes.push_back(new Square(4, 4, 16));
+    shapes.push_back(new Circle(1, 6, 7));
+    shapes.push_back(new Triangle(9, 10, 90));
+
+    cout << "Number of shapes: " << shapes.size() << endl;
+    cout << endl;
+
+    for(Shape* s: shapes)
+    {
+        s->display();
+        cout << endl;
+    }
+
+    for(Shape* s: shapes)
+    {
+        delete s;
+    }
+
+    return 0;
+
+}
